39aa55412904bf8e979f35a06e9cc17e6e012873
Aleksandar Vacić <av@radianttap.com>
2021-02-04 17:04:28 +0100

    SPM is properly usable starting from these v12, so might as well.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
     name: "MKRingProgressView",
-    platforms: [.iOS(.v8), .tvOS(.v9)],
+    platforms: [.iOS(.v12), .tvOS(.v12)],
     products: [
         .library(name: "MKRingProgressView", targets: ["MKRingProgressView"])
     ],
